946e7a60e8ed236477023fc9d3be9c99985c7c02
Suman Dhull <sumandhull@Sumans-Air.domain.name>
2023-01-16 10:40:08 +0530

    backend

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const { connection } = require('./config/db');
+const { todoRoutes } = require('./router/todo.router');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/todos", todoRoutes);
+
+
+
+app.listen(process.env.PORT, async() => {
+    try {
+        await connection;
+        console.log(`Connected to database on port ${process.env.PORT}`);
+    } catch (err) {
+        console.log(err);
+    }
+})
